e9ce8f2d629e86a19a551afd802c0939797e4293
WNota <winnienota1@gmail.com>
2023-06-15 21:46:18 -0700

    Hello puts update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints "Programming using the function puts"
- * return 0.
+ * main - Programming using the function puts
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
