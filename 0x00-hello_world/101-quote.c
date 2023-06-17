bf271fc4117f5d4548557e3502611a402d2ebbf3
WNota <winnienota1@gmail.com>
2023-06-17 08:21:34 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints "A program that prints withouf function
+ * prinf and puts
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
