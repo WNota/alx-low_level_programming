4f32df3fe5a27b31f776d4cd612d75c0227b10fb
WNota <winnienota1@gmail.com>
2023-06-21 15:55:02 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int add(int n1, int n2);
+
+
+int main(void)
+{
+
+	int n = add(89, 9);
+
+	printf("%d\n", n);
+
+	return (0);
+}
