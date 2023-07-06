2072804106af9af59c2adba634dcf4c2a730aa48
WNota <winnienota1@gmail.com>
2023-07-06 10:30:46 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A code that print a string followed by new line
+ * @s: the string to print
+ * Return: Nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+
+		_putchar('\n');
+		return;
+	}
+
+	putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * This is my _putchar.c
+ *
+ */
+
+int _putchar(char c)
+{
+	return write(STDOUT_FILEND, &c, 1);
+}
