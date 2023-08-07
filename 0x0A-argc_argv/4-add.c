b23e229fa74a6cd86c6dc9b4a20763ef0caf036c
WNota <winnienota1@gmail.com>
2023-08-07 12:40:34 -0700

    3-mul.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Prints the multiplication of two args numbers
@@ -11,19 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
 
-	if (argc == 3)
+	if (argc == 1)
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-
+		printf("0\n");
 	return (0);
+	}
+int total = 0;
+for (int i = 1; i < argc; i++)
+{
+	char *num = argv[i];
+	int j = 0;
+
+	while (num[j] != '\0')
+	
+	{
+			if (!isdigit(num[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+			}
+	total += atoi(num);
+}
+printf("%d\n", total);
+return (0);
 }
