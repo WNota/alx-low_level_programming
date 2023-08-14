22bd790407c9309ecd1d03b88a351c991c0412c8
WNota <winnienota1@gmail.com>
2023-08-14 10:03:36 -0700

    dog,h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Dog attributes.
@@ -15,13 +15,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-
 };
 
 /**
  * dog_t - Typedef for dog structure
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
