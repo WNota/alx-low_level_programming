69a3482ad97e05b06c4caab19949bde2be2411ed
WNota <winnienota1@gmail.com>
2023-08-30 12:27:25 -0700

    palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,7 +27,7 @@ int _strlen_recursion(char *s)
 int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
-		return (s);
+	printf("%s is a palindrome.\n", input_string);
 	{
 			if (n1 == n2 || n1 == n2 + 1)
 			return (0);
